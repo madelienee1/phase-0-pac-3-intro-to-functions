85cea08bf6178a4a7481bd3481f10bb913cda593
Madeliene <madelienestiller@gmail.com>
2024-02-23 10:59:03 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Follow along with the examples here
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+
+sayHelloToGuadalupe();
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+
+sayHelloToLiz();
+
